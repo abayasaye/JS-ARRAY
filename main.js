248e21e18f3e84970490eac9c57eb495c1b09ee5
abayasaye <abay400@gmail.com>
2022-05-16 01:08:23 +0300

    array

--- main.js
+++ main.js
@@ -1,96 +1,98 @@
-// var myarray = [ "hello", "w" , 97865, true];
-// console.log(myarray[2]);
-// for (var i=  0 ; i< myarray.length;i++) {
-//     console.log(myarray[i]);
-// }
+var myarray = [ "hello", "w" , 97865, true];
+console.log(myarray[2]);
+for (var i=  0 ; i< myarray.length;i++) {
+    console.log(myarray[i]);
+}
 
-// var sumarray = [ "jacob", "daniel" , "rebeca", "micheal", "sam"];
-// console.log(sumarray)
-// console.log(sumarray[2])
-// for ( var i=0;i<sumarray.length;i++){
-//     console.log(sumarray[i]);
-// }
-//  var numbersarray=[110,307,320,400,500,670,900];
-//  console.log(numbersarray[3]);
-//  for (var i=0;i<numbersarray.length; i++){
-//      console.log(numbersarray[i]);
-//  }
+var sumarray = [ "jacob", "daniel" , "rebeca", "micheal", "sam"];
+console.log(sumarray)
+console.log(sumarray[2])
+for ( var i=0;i<sumarray.length;i++){
+    console.log(sumarray[i]);
+}
+ var numbersarray=[110,307,320,400,500,670,900];
+ console.log(numbersarray[3]);
+ for (var i=0;i<numbersarray.length; i++){
+     console.log(numbersarray[i]);
+ }
 
-//  var array=[10,11,23,25,30,32,37,40,46,50];
-//  var temparray=[];
-//  var tempindex=0;
+ var array=[10,11,23,25,30,32,37,40,46,50];
+ var temparray=[];
+ var tempindex=0;
 
-//  console.log(array)
-//  for (var i=0;i<=array.length;i++){
-//     if (array[i]%2==0){
-//         temparray[tempindex++]=array[i]
-// //         console.log(temparray[1])
-// //         // console.log(array[i])
-// //     }
-// //  }
+ console.log(array)
+ for (var i=0;i<=array.length;i++){
+    if (array[i]%2==0){
+        temparray[tempindex++]=array[i]
+        console.log(temparray[1])
+        // console.log(array[i])
+    }
+ }
 
-// //  var myarray=["hello", 2,"world","jacob",6,7,"bye",98];
-// //  console.log(myarray);
-// //  for (i=0;i<myarray.length;i++){
-// //     if(myarray[i]*1 !=myarray[i]){
-// //        console.log(myarray[i]);
-// //     }
-// //  }
 
-// //  var printarray=[]
-// //  printarray.push(9)
-// //  console.log(printarray)
+ var myarray=["hello", 2,"world","jacob",6,7,"bye",98];
+ console.log(myarray);
+ for (i=0;i<myarray.length;i++){
+    if(myarray[i]*1 !=myarray[i]){
+       console.log(myarray[i]);
+    }
+ }
 
-// //  var numbersarray=[]
-// //  for (var i=0;i<10;i++){
-// //     numbersarray.push(i)
-// //  }
-// //  console.log(numbersarray)
+ 
+ var printarray=[]
+ printarray.push(9)
+ console.log(printarray)
 
-// //  var printarray=[]
-// //  for (var i=0;i<=30;i++){
-// //      if(i%2==0){
+ var numbersarray=[]
+ for (var i=0;i<10;i++){
+    numbersarray.push(i)
+ }
+ console.log(numbersarray)
 
-// //          printarray.push(i)
-// //      }
-// //  }
-// // //  console.log(printarray)
+ var printarray=[]
+ for (var i=0;i<=30;i++){
+     if(i%2==0){
 
-// // var sumarray =[5,3,7,8,11];
-// // var sum=0;
-// // for(var i=0; i<sumarray.length;i++){
-// //     console.log(sumarray[i]);
-// //     sum +=sumarray[i];
-// // }
-// // console.log(sum);
+         printarray.push(i)
+     }
+ }
+//  console.log(printarray)
 
-// // var namesarray=["abayneh","daniel","or","chen","lior"]
-// // for(var i=0;i<namesarray.length;i++){
-// //     console.log(namesarray[i]);
+var sumarray =[5,3,7,8,11];
+var sum=0;
+for(var i=0; i<sumarray.length;i++){
+    console.log(sumarray[i]);
+    sum +=sumarray[i];
+}
+console.log(sum);
 
-// // }
-// function Numbers(num,array){
-//     for (var i=0 ;i<array.length;i++) {
-//         if (num==array[i]){
+var namesarray=["abayneh","daniel","or","chen","lior"]
+for(var i=0;i<namesarray.length;i++){
+    console.log(namesarray[i]);
+
+}
+function Numbers(num,array){
+    for (var i=0 ;i<array.length;i++) {
+        if (num==array[i]){
     
-//             return true
-//         }
-//     }
-//     return false
+            return true
+        }
+    }
+    return false
 
-// }
-// console.log(Numbers(8,[6,9,8,2]))
+}
+console.log(Numbers(8,[6,9,8,2]))
 
 
-// function myarray(arrayNum){
-//     var save=0
-//     for (var i=0; i<arrayNum.length;i++){
-//         if(arrayNum[i]%2==0){
-//             save++;
-//         }
-//     }
-//     console.log(save)
-// }
+function myarray(arrayNum){
+    var save=0
+    for (var i=0; i<arrayNum.length;i++){
+        if(arrayNum[i]%2==0){
+            save++;
+        }
+    }
+    console.log(save)
+}
 // myarray([4,5,7,8,10,12]);
 
 function array(array1,array2){
@@ -104,5 +106,17 @@ return console.log (false);
 
 
 array([5,4,7,9],[2,10,8,6,11])
+
+function myFunc(arrayA){
+    var maxArray = 0;
+    for (var i=0 ;i<arrayA.length;i++){
+        if (arrayA[i]>maxArray){
+            maxArray=arrayA[i]
+        }
+    }
+    return maxArray
+}
+console.log(myFunc([4,5,8,9]))
+
                 
             
